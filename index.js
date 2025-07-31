7620e6f65101549bda990235e0e106728a9ce9bc
thej <thej.devops01@gmail.com>
2025-07-31 11:32:03 +0530

    c

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 // index.js
-require('dd-trace').init(); // Datadog APM
-const bugsnag = require('@bugsnag/js');
-const bugsnagClient = bugsnag('YOUR_API_KEY');
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, DevOps!');
+  res.send('Hello, World!');
 });
 
-app.use(bugsnagClient.requestHandler);
-app.use(bugsnagClient.errorHandler);
-
-app.listen(3000, () => console.log('Server started on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
